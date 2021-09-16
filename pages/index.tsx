9e3eb9a54ef5bc87c45e0a2e4ce951eab506548d
上森香 <uemori.kaoru@adways.net>
2021-09-16 23:32:33 +0900

    :sparkles: mypage作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
+      <div>
       <h1>えぺとも</h1>
+  　　　<Link href={`/mypage`}><a>マイページへ</a></Link>
+      </div>
   )
 }
---
+++ pages/mypage/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function MyPage() {
+    return (
+        <>
+            <h1>MyPageだよ</h1>
+            <Link href={`/`}><a>トップへ</a></Link>
+        </>
+    )
+}
